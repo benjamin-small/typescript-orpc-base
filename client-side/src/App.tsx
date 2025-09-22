f993256c04afcbf6cd699b8ce7bf3ed41a607cb4
Benjamin Small <benjamin.small83@gmail.com>
2025-09-22 09:11:24 -0600

    tweaks

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -44,14 +44,11 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-                {planets.map((v) => {
-                    return <>
-                        {v.name}
-                    </>
-                })}
-            </p>
+            {planets.map((v) => {
+                return <div>
+                    {v.name}({v.id}) - {v.description}
+                </div>
+            })}
         </>
     )
 }
